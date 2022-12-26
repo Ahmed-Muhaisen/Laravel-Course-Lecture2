236b9d3a5a5d5c0045636b3188138e41a76328b3
Ahmed-Muhaisen <amhysn574@gmail.com>
2022-12-26 10:16:57 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,101 @@
+<?php 
+
+$name ="Mohammed";
+$name =6;
+
+
+
+// echo "<h1> Hi $name </h1>";
+// echo '<h1> Hi ' .$name . '</h1>';
+
+
+//   $i=30;
+
+
+//   if($i>50 || $i==50){
+//     echo "أنت في أخر أيامك";
+// }
+
+// else if($i<50  && $i>30){
+//     echo "أنت في شبابك ";
+
+// }
+
+// else {
+//     echo "أنت لسة بيبي ";
+
+// }
+
+
+// 1-Array
+// 1-Index Array
+// 2- Multydiminition Array
+// 3- Assiciativ Array *******
+
+// echo '<br>';
+
+// $names =['Ahmed',"Mohammed","Ail"];
+// // var_dump($names) ;
+
+// $name =[
+// ['mohammed','Ali' ,'Sara'],
+// ['omer','Asmaa' ,'Ahmed'],
+// ['Mahmoud','Ali' ,'Ablalash'],
+// ['faraj','bara' ,'sara' ,['Ahmed','Khlel','saly',[11]],'Asdsds']
+// ];
+ 
+// echo $name[3][4];
+
+// 1-for
+
+// 2-while
+
+// 3-Do .... while
+
+// 4-foreach
+
+
+// for( $h=1 ; $h < 11 ; $h++ ){
+
+// echo "<br> $h" ;
+
+// }
+
+// $i =3 ;
+// while($i<=5){
+
+// echo "$i <br>" ;
+
+// $i++;
+// }
+
+// $i =50 ;
+// do{
+//  echo "$i <br>" ;
+// $i++;
+// }
+// while($i<3)
+
+// $age =[20,21,38,19,20,18];
+// var_dump($age);
+
+$age =[
+'ebraheem' => 20 ,
+'faraj' => 21 ,
+'hafez'=>38,
+
+]
+; 
+
+echo $age[0];
+
+
+  
+
+
+
+
+
+
+
+?>
